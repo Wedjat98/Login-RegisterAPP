8c35cbeb3558268340ab5f01383af3b2349943cb
lionlemon <ame9807@gmail.com>
2021-07-24 16:40:23 +0900

    login-register

--- service/db/db.js
+++ service/db/db.js
@@ -2,7 +2,7 @@ module.exports = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: '123456',
+        password: 'admin',
         port: '3306',
         database: 'login'
     }
